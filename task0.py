3aef1d45ec773f18bbf39130f613d3c79b97cb8f
MR.R <ivanryabovg@gmail.com>
2023-03-26 23:47:57 +0300

    seminar1

---
+++ task0.py
@@ -0,0 +1,11 @@
+# найти большое число из 2-х
+a=int(input('введите число1 '))
+b=int(input('введите число2 '))
+max=a
+if a>b:
+    print(max)
+elif a==b:
+    print("числа равны"5)
+else:
+    max=b
+    print(max)
